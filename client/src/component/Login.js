d77e2cd2a7f3676f3b44d7cf1bdcb3a620e21636
muthuanushyam <muthuanushyamofficial3@gmail.com>
2024-11-03 12:07:16 +0530

    Update Login.js

--- client/src/component/Login.js
+++ client/src/component/Login.js
@@ -10,7 +10,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("http://localhost:8000/login", {
+      const response = await axios.post("https://todolistreactapp-server.onrender.com/login", {
         username,
         password,
       });
