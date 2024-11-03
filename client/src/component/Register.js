e9bc430cf5c2b02c873373ba74c01397f1a59d84
muthuanushyam <muthuanushyamofficial3@gmail.com>
2024-11-03 12:07:41 +0530

    Update Register.js

--- client/src/component/Register.js
+++ client/src/component/Register.js
@@ -10,7 +10,7 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("http://localhost:8000/register", {
+      const response = await axios.post("https://todolistreactapp-server.onrender.com/register", {
         username,
         password,
       });
